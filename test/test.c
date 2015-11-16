0fe06661cbbfa831d71387203bcbce5fa0700d2a
jobtalle <jobtalle@hotmail.com>
2015-11-16 17:36:37 +0100

    Test case loads & shows image

--- test/test.c
+++ test/test.c
@@ -3,10 +3,55 @@
 #include <ccore/opengl.h>
 
 #include <stdio.h>
+#include <gl/GL.h>
 
+#include <lodepng/lodepng.h>
 #include <convexHull/convexHull.h>
 
-#define TITLE "Convext Hull test case\n"
+#define TITLE "Convex Hull test case\n"
+#define IMAGEDIR "test/source.png"
+
+unsigned char *pixels;
+GLuint texture;
+
+void loadImage()
+{
+	unsigned int width, height;
+
+	// Load image from file using lodepng
+	lodepng_decode32_file(&pixels, &width, &height, IMAGEDIR);
+	printf("\tLoaded test image %dx%d\n", width, height);
+
+	// Initialize openGL texture
+	glEnable(GL_TEXTURE_2D);
+	glDisable(GL_DEPTH_TEST);
+	glDisable(GL_LIGHTING);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+}
+
+void render()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 0.0f);	glVertex2f(-1.0f, 1.0f);
+	glTexCoord2f(0.0f, 1.0f);	glVertex2f(-1.0f, -1.0f);
+	glTexCoord2f(1.0f, 1.0f);	glVertex2f(1.0f, -1.0f);
+	glTexCoord2f(1.0f, 0.0f);	glVertex2f(1.0f, 1.0f);
+	glEnd();
+}
 
 int run()
 {
@@ -29,6 +74,7 @@ int run()
 	}
 
 	// Render
+	render();
 	ccGLBuffersSwap();
 
 	return 1;
@@ -47,6 +93,12 @@ int main(int argc, char **argv)
 
 	// Bind openGL context
 	ccGLContextBind();
+
+	// Configure opengl
+	glClearColor(0.3f, 0.3f, 0.8f, 1.0f);
+
+	// Initialize
+	loadImage();
 
 	while(run());
 
