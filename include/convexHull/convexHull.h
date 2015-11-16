ff66c0a7e85c603656a9e1096d0dd140d7ed2476
jobtalle <jobtalle@hotmail.com>
2015-11-16 17:17:42 +0100

    Added license

--- include/convexHull/convexHull.h
+++ include/convexHull/convexHull.h
@@ -1,2 +1,14 @@
-#pragma once
+//__________________________________________________________________________________//
+//                                                                                  //
+//                                Convex Hull V1.0                                  //
+//                           Written by Job Talle (2015)                            //
+//__________________________________________________________________________________//
+//                                                                                  //
+//      This program is free software: you can redistribute it and/or modify        //
+//      it under the terms of the 3-clause BSD license.                             //
+//                                                                                  //
+//      You should have received a copy of the 3-clause BSD License along with      //
+//      this program. If not, see <http://opensource.org/licenses/>.                //
+//__________________________________________________________________________________//
 
+#pragma once
